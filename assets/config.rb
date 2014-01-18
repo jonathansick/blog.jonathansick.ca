d7189bc050300b3ed68070d406711edd7ccde465
Jonathan Sick <jonathansick@mac.com>
2014-01-18 18:08:32 -0500

    Update layout to Susy-Next

--- assets/config.rb
+++ assets/config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
-require "breakpoint", "susy"
+require "breakpoint"
+require "susy"
 
 # Set this to the root of your project when deployed:
 http_path = "/assets/"
